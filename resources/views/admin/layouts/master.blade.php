eeeb28ab0df212449e57017cbae3902e0548f3b7
SYNRakuza <ikbalphoenix@gmail.com>
2020-01-04 01:20:32 +0800

    remove template..

--- resources/views/admin/layouts/master.blade.php
+++
@@ -1,9 +0,0 @@
-@include('admin.layouts.includes._header')
-
-@include('admin.layouts.includes._navbar')
-
-@include('admin.layouts.includes._sidebar')
-
-@yield('content')
-
-@include('admin.layouts.includes._script')
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('admin.dashboard.index');
+    return view('welcome');
 });
