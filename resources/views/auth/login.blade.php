18f7fe5fb8e610a22b2733a6309df5c8662ea4cd
herulhidayat <khaerulhidayatherul@gmail.com>
2020-01-04 23:57:00 +0800

    no message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,10 +28,10 @@
                             <div class="card-body">
                                 <div class="logo text-center">
                                     <a href="index.html">
-                                        <img src="{{asset('assets/images/f-logo.png')}}" alt="">
+                                        <img src="{{asset('assets/images/unismuh.png')}}" height="70" width="70" alt="">
                                     </a>
                                 </div>
-                                <h4 class="text-center m-t-15">Log into Your Account</h4>
+                                <h4 class="text-center m-t-15">Aplikasi Monitoring Absensi Universitas Muhammadiyah Makassar</h4>
                                 <form class="m-t-30 m-b-30" action="/postlogin" method="post">
                                 {{csrf_field()}}
                                     <div class="form-group">
