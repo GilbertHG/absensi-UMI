d21fe883fe8a9f845ec7889cf8e15a238d26444f
herulhidayat <khaerulhidayatherul@gmail.com>
2020-01-25 19:25:57 +0800

    tampilan pass

--- resources/views/admin/dashboard/changePassword.blade.php
+++ resources/views/admin/dashboard/changePassword.blade.php
@@ -6,43 +6,39 @@
         <!-- row -->
         <div class="row">
             <div class="col-12">
-                {{-- @if($message = Session::get('sukses'))
-                        <div class="alert alert-success">{{$message}}</div>
-            @endif --}}
-            {{-- @error('nip_nbm_dosen')
-                        <div class="alert alert-danger">{{ $message }}
-        </div>
-        @enderror --}}
-        <div class="card">
-            <div class="card-body">
-                <div class="float-left">
-                    <h4 class="card-title mdi mdi-image-filter-none f-s-20"> Ganti Password</h4>
-                </div>
-                <div class="floatright">
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2">
-                            <div class="panel panel-default">
-                                <br><br>
+                    {{-- @if($message = Session::get('sukses'))
+                            <div class="alert alert-success">{{$message}}</div>
+                @endif --}}
+                {{-- @error('nip_nbm_dosen')
+                            <div class="alert alert-danger">{{ $message }}
+            </div>
+            @enderror --}}
+                <div class="card">
+                    <div class="card-body">
+                        <div class="float-left">
+                            <h4 class="card-title mdi mdi-image-filter-none f-s-20"> Ganti Password</h4>
+                        </div>
+                    </div>
+                    <div class="card-body">
 
-                                <div class="panel-body">
-                                    @if (session('error'))
-                                    <div class="alert alert-danger">
-                                        {{ session('error') }}
-                                    </div>
-                                    @endif
-                                    @if (session('success'))
-                                    <div class="alert alert-success">
-                                        {{ session('success') }}
-                                    </div>
-                                    @endif
-                                    <form class="form-horizontal" method="POST" action="{{ route('changePassword') }}">
-                                        {{ csrf_field() }}
+                            <div class="panel-body">
+                                @if (session('error'))
+                                <div class="alert alert-danger">
+                                    {{ session('error') }}
+                                </div>
+                                @endif
+                                @if (session('success'))
+                                <div class="alert alert-success">
+                                    {{ session('success') }}
+                                </div>
+                                @endif
+                                <form class="form-horizontal" method="POST" action="{{ route('changePassword') }}">
+                                    {{ csrf_field() }}
+                                    <div class="row">
+                                        <div class="col form-group{{ $errors->has('current-password') ? ' has-error' : '' }}">
+                                            <label for="new-password" class="control-label">Password Lama</label>
 
-                                        <div
-                                            class="form-group{{ $errors->has('current-password') ? ' has-error' : '' }}">
-                                            <label for="new-password" class="col-md-4 control-label">Password Lama</label>
-
-                                            <div class="col-md-6">
+                                            <div>
                                                 <input id="current-password" type="password" class="form-control"
                                                     name="current-password" required>
 
@@ -54,10 +50,10 @@
                                             </div>
                                         </div>
 
-                                        <div class="form-group{{ $errors->has('new-password') ? ' has-error' : '' }}">
-                                            <label for="new-password" class="col-md-4 control-label">Password Baru</label>
+                                        <div class="col form-group{{ $errors->has('new-password') ? ' has-error' : '' }}">
+                                            <label for="new-password" class="control-label">Password Baru</label>
 
-                                            <div class="col-md-6">
+                                            <div>
                                                 <input id="new-password" type="password" class="form-control"
                                                     name="new-password" required>
 
@@ -69,32 +65,30 @@
                                             </div>
                                         </div>
 
-                                        <div class="form-group">
-                                            <label for="new-password-confirm" class="col-md-4 control-label">Ulang Password Baru</label>
+                                        <div class="col form-group">
+                                            <label for="new-password-confirm" class="control-label">Ulang Password Baru</label>
 
-                                            <div class="col-md-6">
+                                            <div>
                                                 <input id="new-password-confirm" type="password" class="form-control"
                                                     name="new-password_confirmation" required>
                                             </div>
                                         </div>
 
-                                        <div class="form-group">
-                                            <div class="col-md-6 col-md-offset-4">
-                                                <button type="submit" class="btn btn-primary">
-                                                    Ubah Password
-                                                </button>
-                                            </div>
+                                    </div>
+                                    <div class="form-group float-right">
+                                        <div class="col-md-6 col-md-offset-4">
+                                            <button type="submit" class="btn btn-primary">
+                                                Ubah Password
+                                            </button>
                                         </div>
-                                    </form>
-                                </div>
-                            
-                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 <!-- #/ container -->
-</div>
+    </div>
 @endsection
